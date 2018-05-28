7235658c5eb58108815e306791d68988f33b3e09
mtotodev05 <mtotodev05@gmail.com>
2018-05-28 21:22:42 +0300

    Implemented activities

--- app/src/main/java/dev/brian/com/dashboard/Home.java
+++ app/src/main/java/dev/brian/com/dashboard/Home.java
@@ -1,13 +1,50 @@
 package dev.brian.com.dashboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
-public class Home extends AppCompatActivity {
-
+public class Home extends AppCompatActivity implements View.OnClickListener {
+    private CardView cardBank,cardIdeas,cardAttachment,cardLinks,cardWifi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        cardBank = (CardView)findViewById(R.id.card_banking);
+        cardIdeas = (CardView)findViewById(R.id.card_ideas);
+        cardAttachment = (CardView) findViewById(R.id.card_attachment);
+        cardLinks = (CardView)findViewById(R.id.card_links);
+        cardWifi = (CardView) findViewById(R.id.card_wifi);
+
+        //Adding ActionListeners to the Cards.
+
+        cardBank.setOnClickListener(this);
+        cardIdeas.setOnClickListener(this);
+        cardAttachment.setOnClickListener(this);
+        cardLinks.setOnClickListener(this);
+        cardWifi.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch(view.getId()){
+            case R.id.card_banking:
+                intent = new Intent(this,Bank.class);
+                startActivity(intent);
+                break;
+            case R.id.card_ideas:
+                intent = new Intent(this,Bank.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+
+
+
+
+        }
     }
 }
